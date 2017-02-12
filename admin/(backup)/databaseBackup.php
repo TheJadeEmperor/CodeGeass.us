108fad7b8ff685e638d478f7f0265f229d82a4b8
TheJadeEmperor <louie.online.acc@gmail.com>
2017-02-11 20:56:42 -0500

    delete backup and error_log files

--- admin/(backup)/databaseBackup.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$date = date('Y-m-d', time());
-
-$command = 'mysqldump -ucodegeas_backup -pMilitary1! codegeas_smf > /home6/codegeas/forumBackups/'.$date.'.sql';
-
-system($command);
-
-?>
--- admin/fanlisting/(backup)/whatis.php
+++
@@ -1,8 +0,0 @@
-<?
-$context[fanlist][whatis] = '<p>A fanlisting is simply an online listing of fans of a subject, such as a TV show, 
-actor, or musician, that is created by an individual and open for fans from around the world to join. There are 
-no costs, and the only requirements to join a fanlisting are your name and country. Fanlistings do not have 
-to be large sites (although some are) - they are just a place where you can have your name listed along with 
-other fans of the same subject. TheFanlistings.org is the original (but not only) web directory for fanlistings, 
-dedicated to uniting fans across the globe. (Taken from TheFanlistings.org)</p>';
-?>
