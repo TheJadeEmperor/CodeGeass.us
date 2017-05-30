07fd9ae45a0dc8f8d789c9173d4778174d4fc322
TheJadeEmperor <louie.online.acc@gmail.com>
2017-05-30 00:16:53 -0400

    update menu

--- include/menu.php
+++ include/menu.php
@@ -2,39 +2,36 @@
 function mainMenu($menu) {
     $content = '<ul class="pureCssMenu pureCssMenum">';
     
-    foreach($menu[item] as $name => $value) {
-        if(sizeof($value[subMenu]) > 0) { //if there is a sub-menu
+    foreach($menu['item'] as $name => $value) {
+        if(sizeof($value['subMenu']) > 0) { //if there is a sub-menu
         
             $content .= '<li class="pureCssMenui" title="'.$value['title'].'">
-            <a class="pureCssMenui" href="'.$value[link].'"><span>'.$name.'</span></a>
+            <a class="pureCssMenui" href="'.$value['link'].'"><span>'.$name.'</span></a>
             <ul class="pureCssMenum">';
           
-            foreach($value[subMenu] as $name1 => $val1) {
-                if(sizeof($val1[subMenu]) > 0) //sub-menu
-                {
-                    $content .= '<li class="pureCssMenui" title="'.$val1[title].'">
-                    <a class="pureCssMenui" href="'.$val1[link].'"><span>'.$name1.'</span></a>
+            foreach($value['subMenu'] as $name1 => $val1) {
+                if(sizeof($val1['subMenu']) > 0) { //sub-menu
+                    $content .= '<li class="pureCssMenui" title="'.$val1['title'].'">
+                    <a class="pureCssMenui" href="'.$val1['link'].'"><span>'.$name1.'</span></a>
                     <ul class="pureCssMenum">';
                     
-                    foreach($val1[subMenu] as $name2 => $val2)
+                    foreach($val1['subMenu'] as $name2 => $val2)
                     {
-                         $content .= '<li class="pureCssMenui" title="'.$val2[title].'">
-                         <a class="pureCssMenui" href="'.$val2[link].'">'.$name2.'</a></li>';
+                         $content .= '<li class="pureCssMenui" title="'.$val2['title'].'">
+                         <a class="pureCssMenui" href="'.$val2['link'].'">'.$name2.'</a></li>';
                     }
                     
                     $content .= '</ul></li>';
                 }
-                else 
-                {
-                    $content .= '<li class="pureCssMenui" title="'.$value[title].'">
-                    <a class="pureCssMenui" href="'.$val1[link].'">'.$name1.'</a></li>';
+                else {
+                    $content .= '<li class="pureCssMenui" title="'.$value['title'].'">
+                    <a class="pureCssMenui" href="'.$val1['link'].'">'.$name1.'</a></li>';
                 }
             }
             $content .= '</ul>
             </li>';
         }
-        else
-        {
+        else {
             $content .= '<li class="pureCssMenui" title="'.$value[title].'">
             <a class="pureCssMenui" href="'.$value[link].'">'.$name.'</a></li>';
         }
@@ -47,8 +44,7 @@ function mainMenu($menu) {
 
 function googleSearch($dir) {		
 	$search = '<fieldset title="Code Geass Search"> 
-	<legend align="center"><a href = "'.$_SERVER['PHP_SELF'].'"><b>Code Geass Search</b></a>
-	</legend>
+	<legend align="center"><a href = "'.$_SERVER['PHP_SELF'].'"><b>Code Geass Search</b></a></legend>
 	<form action="http://www.google.com/cse" id="cse-search-box">
 	<table>
 	<tr valign="top">
@@ -79,28 +75,28 @@ $menu = array(
     'Home' => array(
         'link' => $dir,
         'title' => 'Code Geass'),
-	'The Empire' => array(
-            'link' => 'javascript:void',
-            'title' => 'About the Website',
-            'subMenu' => array(
-                'Version 1.0' => array(
-                    'link' => $dir.'about/version1.php',
-                    'title' => 'Version 1 of Refrain'),
-                'Version 2.0' => array(
-                    'link' => $dir.'about/version2.php',
-                    'title' => 'Version 2 of Refrain'),
-                'Allies' => array(
-                    'link' => $dir.'allies/', 
-                    'title' => 'Allies', 
-                    'subMenu' => array(
-                        'Allies List' => array(
-                            'link' => $dir.'allies/list.php'),
-                        'Apply Here' => array(
-                            'link' => $dir.'allies/'),
-                        'Marketing Division' => array()
-                    )
-                )
-            )
+	'About Us' => array(
+		'link' => 'javascript:void',
+		'title' => 'About the Website',
+		'subMenu' => array(
+			'Version 1.0' => array(
+				'link' => $dir.'about/version1.php',
+				'title' => 'Version 1 of Refrain'),
+			'Version 2.0' => array(
+				'link' => $dir.'about/version2.php',
+				'title' => 'Version 2 of Refrain'),
+			'Allies' => array(
+				'link' => $dir.'allies/', 
+				'title' => 'Allies', 
+				'subMenu' => array(
+					'Allies List' => array(
+						'link' => $dir.'allies/list.php'),
+					'Apply Here' => array(
+						'link' => $dir.'allies/'),
+					'Marketing Division' => array()
+				)
+			)
+		)
 	),
 	'Characters' => array(
 		'link' => $dir.'chars.php',
@@ -124,19 +120,19 @@ $menu = array(
 		)
 	),
 	'Area 11' => array(
-            'link' => 'javascript:void',
-            'title' => 'The Code Geass Library',
-            'subMenu' => array(
-                'The Power of Kings' => array(
-                    'link' => $dir.'about/',
-                    'title' => 'About the Geass'),
-                'Locations & Groups' => array(
-                    'link' => $dir.'about/affiliation/',
-                    'title' => 'Code Geass Groups'),
-            'Code Geass Manga' => array(
-                'link' => $dir.'about/manga.php',
-                'title' => 'Code Geass Manga'),    
-            )
+		'link' => 'javascript:void',
+		'title' => 'The Code Geass Library',
+		'subMenu' => array(
+			'The Power of Kings' => array(
+				'link' => $dir.'about/',
+				'title' => 'About the Geass'),
+			'Locations & Groups' => array(
+				'link' => $dir.'about/affiliation/',
+				'title' => 'Code Geass Groups'),
+		'Code Geass Manga' => array(
+			'link' => $dir.'about/manga.php',
+			'title' => 'Code Geass Manga'),    
+		)
 	),
 	'Media' => array(
 		'link' => 'javascript:void',
@@ -246,53 +242,82 @@ $menu = array(
                     "Ep 1 - The Day of the Demon's Birth" => array(
                         'link' => $dir.'episodes/ss/1_1.php',
                         'title' => "The Day of the Demon's Birth"
-                    ),
-                    'Ep 2 - The White Knight Awakens' => array(
+						),
+                    "Ep 2 - The White Knight Awakens" => array(
                         'link' => $dir.'episodes/ss/1_2.php',
                         'title' => "The White Knight Awakens"
-                    ),
-                    'Ep 3 - The Fake Classmate' => array(
+						),
+                    "Ep 3 - The Fake Classmate" => array(
                         'link' => $dir.'episodes/ss/1_3.php',
-                        'title' => 'The Fake Classmate')
-                )
+                        'title' => 'The Fake Classmate'
+						),
+					"Ep 4 - His Name is Zero" => array(
+                        'link' => $dir.'episodes/ss/1_4.php',
+                        'title' => "His Name is Zero"
+						),
+					"Last Ep - Zero" => array(
+                        'link' => $dir.'episodes/ss/1_25.php',
+                        'title' => "Zero"
+						)
+				)
             ),
             'Season 2 Gallery' => array(
                 'link' => $dir.'season_2.php',
                 'title' => 'The Zero Requiem', 
                 'subMenu' => array(
-                    'Ep 1 - The Day A Demon Awakens' => array(
+                    "Ep 1 - The Day A Demon Awakens" => array(
                         'link' => $dir.'episodes/ss/2_1.php',
                         'title' => "The Day A Demon Awakens"
-                    ),
-                    'Ep 2 - Plan For Independent Japan' => array(
+						),
+                    "Ep 2 - Plan For Independent Japan" => array(
                         'link' => $dir.'episodes/ss/2_2.php',
                         'title' => 'Plan For Independent Japan'
-                    ),
-                    'Ep 3 - Imprisoned in Campus' => array(
+						),
+					"Ep 3 - Imprisoned in Campus" => array(
                         'link' => $dir.'episodes/ss/2_3.php',
-                        'title' => 'Imprisoned in Campus')
+                        'title' => 'Imprisoned in Campus'
+						),
+					"Ep 4 - Counterattack at the Gallows" => array(
+                        'link' => $dir.'episodes/ss/2_4.php',
+                        'title' => "Counterattack at the Gallows"
+						),
+					"Last Ep - Re;" => array(
+                        'link' => $dir.'episodes/ss/2_25.php',
+                        'title' => "Re;"
+						),
                 )  
             ),
 			'Wallpapers' => array(
 				'link' => $dir.'media/wallpapers/',
-				'title' => 'Code Geass Wallpapers'),
+				'title' => 'Code Geass Wallpapers',
+				  'subMenu' => array(
+					"2008 Wallpapers" => array(
+						'link' => $dir.'media/wallpapers/2008_calendar.php',
+						'title' => '2009 Calendar'
+					),
+					"2009 Wallpapers" => array(
+						'link' => $dir.'media/wallpapers/2009_calendar.php',
+						'title' => '2009 Calendar'
+					),
+					)
+				),
 			), 
     ),
     'Knightmares' => array(
         'link' => $dir.'knightmares/',
         'title' => 'Code Geass Knightmare Frames'),
     
-    'Chat' => array(
-        'link' => $dir.'chat/',
-        'title' => 'Chatroom'),
-	'Forum' => array(
+    'Season 3' => array(
+        'link' => $dir.'season_3.php',
+        'title' => 'Code Geass R3'),
+	'Chat' => array(
         'link' => 'https://www.facebook.com/groups/cgrefrain3/',
-        'title' => 'Anime Forum'),
+        'title' => 'Refrain on Facebook'),
 )//item
 );//$menu
 
 //meta tags
-$context[meta] = $meta = $section[$key][meta];
+$context['meta'] = $meta = $section[$key]['meta'];
 
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN""http://www.w3.org/TR/html4/loose.dtd">
@@ -300,8 +325,8 @@ $context[meta] = $meta = $section[$key][meta];
 <head><?=$redir?>
 <title><?=$meta[title]?></title>
 <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
-<meta name="keywords" content="<?=$meta[tags]?>">
-<meta name="description" content="<?=$meta[desc]?>">
+<meta name="keywords" content="<?=$meta['tags']?>">
+<meta name="description" content="<?=$meta['desc']?>">
 <meta name="author" content="The Emperor">
 <meta name="google-site-verification" content="fbXXyQMtiDc80jDwOq2rRqCDY8R5n6PvIVqo3itDkFI"><!-- Google verification -->
 <link href = "<?=$dir?>include/css/popup.css" rel="stylesheet" type="text/css">
