75b5bb98a72575bd438278643e212d5acd4b99e6
TheJadeEmperor <louie.online.acc@gmail.com>
2018-01-05 01:08:40 -0500

    update download links for ost's

--- media/music/ost.php
+++ media/music/ost.php
@@ -2,27 +2,23 @@
 $dir = '../../';
 include($dir.'media/music/musicCode.php');
 
-function listSound($ost, $season, $track)
-{   
+function listSound($ost, $season, $track) {   
     global $dir, $links;
     
     $ost = $season.'_'.$track;
+	
     //read all files in directory
-    if ($handle = opendir('../downloads/ost_'.$ost))    
-    {
+    if ($handle = opendir($dir.'media/downloads/ost_'.$ost)) {
         //List all the files
-        while (false !== ($file = readdir($handle)))
-        {
-            if($file != "Thumbs.db" && $file != ".." && $file != ".")
-            {   
+        while (false !== ($file = readdir($handle))) {
+            if($file != "Thumbs.db" && $file != ".." && $file != ".") {   
                 $ostFile[] = $file;
             }//if
         }//while
 		
 		sort($ostFile);
 		
-		foreach($ostFile as $file)
-		{
+		foreach($ostFile as $file) {
 		    $fileDisplay = str_replace('_', ' ', $file);
             $fileDisplay = str_replace('.mp3', '', $fileDisplay);
             $fileDisplay = str_replace('Code Geass - ', '', $fileDisplay);
@@ -32,11 +28,10 @@ function listSound($ost, $season, $track)
             $fileDisplay = str_replace('R2 - ', '', $fileDisplay);
             
             $trackNumber = 'ost_'.$season.'_'.$track;
-            $fileLink = $links[ $trackNumber ][link];
-            
-            $content .= '<a href="'.$links[$trackNumber][link].'" target=_blank>'.$fileDisplay.'</a><br /><br />';
+            $fileLink = $dir.'media/downloads/ost_'.$ost.'/'.$file; 
+			
+            $content .= '<a href="'.$fileLink.'" target=_blank>'.$fileDisplay.'</a><br /><br />';
 		}
-		
     }//if
     
     closedir($handle);
@@ -67,8 +62,7 @@ echo '<table>
 	   <div>'.listSound($ost, 1, 1).'</div>
 	   </td>
 	<td align="center">
-		<a href="'.$links[ost_1_1]['link'].'" target=_blank><img src="'.$dir.'media/music/ost/ost_1.jpg" 
-		alt="'.$links[ost_1_1][title].'" title="'.$links[ost_1_1][title].'" /></a>
+		<a href="'.$dir.'media/music/ost/ost_1.jpg" target="_blank"><img src="'.$dir.'media/music/ost/ost_1.jpg" title="Code Geass OST" /></a>
 	</td>
 </tr><tr valign="top" id="track2">
 	<td>
@@ -76,8 +70,7 @@ echo '<table>
 	   <div>'.listSound($ost, 1, 2).'</div>
 	   </td>
 	<td>
-		<a href="'.$links[ost_2_1]['link'].'" target=_blank><img src="'.$dir.'media/music/ost/ost_2.jpg"
-		alt="'.$links[ost_2_1][title].'"  title="'.$links[ost_2_1][title].'" /></a>
+		<a href="'.$dir.'media/music/ost/ost_2.jpg" target=_blank><img src="'.$dir.'media/music/ost/ost_2.jpg" title="Code Geass OST" /></a>
 	</td>
 </tr>
 </table>
@@ -97,22 +90,13 @@ echo '<table>
 		</div>
 	</td>
 </tr>
-</table>
-
-
-';
+</table>';
 ?>
+
 <div class="content">
 <?=top()?><p>Music soundtracks donated by Kozuki Luna </p>
 <p>Thank you Kozuki Luna for the contributions.</p>
 </div>
-
-<div class="moduleBlack"><h2>Download All OST's</h2>
-<div>
-    <p>You can download all these OST's and more on our downloads page.</p>
     
-    <p><a href="<?=$dir?>media/downloads">Click here to go to the downloads page</a></p>
-</div>
-</div>    
 <?
 include($dir.'include/bottom.php'); ?>
