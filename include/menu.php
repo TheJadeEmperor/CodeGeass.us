b19460fd3814db0b0edb5d1baa3b28128b9b4a64
TheJadeEmperor <benjamin_louie@drugfree.org>
2021-06-29 16:36:54 -0400

    banner ad for header

--- include/menu.php
+++ include/menu.php
@@ -374,6 +374,18 @@ flush();
 <div id="container">
     <?=mainMenu($menu)?>
 
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- CodeGeass Header -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-9979225970120201"
+     data-ad-slot="9089608410"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+	
     <p>&nbsp;</p>
     <p>&nbsp;</p>
     <p>&nbsp;</p>
