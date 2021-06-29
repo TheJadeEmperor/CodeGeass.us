4f60b0975208de3a0793e78aeb3fe364173876f7
TheJadeEmperor <benjamin_louie@drugfree.org>
2021-06-29 10:00:44 -0400

    adsense tag & analytics tag

--- include/menu.php
+++ include/menu.php
@@ -346,18 +346,20 @@ function popUp(url) {
     window.open(url, "Code Geass Image", "location=1,status=1,scrollbars=1,width=950,height=690");
 }
 </script>
-<script type="text/javascript">
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-32959141-1']);
-  _gaq.push(['_trackPageview']);
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-N1R8SEELKM"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })(); 
+  gtag('config', 'G-N1R8SEELKM');
 </script>
+
+<!-- Adsense -->
+<script data-ad-client="ca-pub-9979225970120201" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
 </head>
 <?
 flush(); 
