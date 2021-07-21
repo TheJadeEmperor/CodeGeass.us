ea927c28a295a40763f30ed6397fbe141bb1514c
TheJadeEmperor <benjamin_louie@drugfree.org>
2021-07-21 10:56:06 -0400

    google ads file

--- media/music/musicCode.php
+++ media/music/musicCode.php
@@ -15,11 +15,7 @@ $section = array(
 		'tags' => 'code geass music, code geass ost, code geass sound episodes',
 		'title' => 'Code Geass Music | Code Geass Media'),
 	'leftBox' => "<h2>Music Main Menu</h2><p>
-		The proud members of the Anime Empire have compiled a list of the entire Code Geass Soundtrack! 
-		Desperate and frustrated looking for lyrics of Code Geass songs all over the web? Your search ends here! 
-		We have the lyrics, names, artists, and downloads available! If you are unable to find a song that you're 
-		looking for, please sign up and send in a request for us on the forum, we will get it on here as soon 
-		as possible!!</p>",
+		The proud members of the Anime Empire have compiled a list of the entire Code Geass Soundtrack! Desperate and frustrated looking for lyrics of Code Geass songs all over the web? Your search ends here! We have the lyrics, names, artists, and mp3's available! If you have any questions or concerns, contact us at our support email in the footer.</p>",
 	'display' => 'Code Geass Music',
 	'title' => 'Code Geass Music',
 	'link' => $dir.'media/music'),
@@ -28,10 +24,7 @@ $section = array(
 		'tags' => 'code geass album covers, code geass music',
 		'title' => 'Code Geass Album Covers | Code Geass Media'),
 	'leftBox' => "<h2>Album Covers</h2><p>This is your one-stop source for 
-		all your <b>Code Geass</b> music related needs. Ever wonder where to find the lyrics of your 
-		favorite song? Then look no further, because we have it all! Even if we don't have what you're 
-		looking for, we will have it eventually, in our continuing effort to make this website the
-		place for anything <b>Code Geass</b> related.</p>",
+		all your <b>Code Geass</b> music related needs. Ever wonder where to find the lyrics of your favorite song? Then look no further, because we have it all! If you have any questions or concerns, contact us at our support email in the footer.</p>",
 	'display' => 'Album Covers',
 	'title' => 'Code Geass Album Covers',
 	'link' => $dir.'media/music/album.php'),
@@ -43,9 +36,7 @@ $section = array(
 		Desperate and frustrated looking for lyrics of Code Geass songs all over the web? Your search ends here! ',
 		),
 	'leftBox' => "<h1>Code Geass OSTs</h1><h2>R1 & R2 OST's</h2>".$leftBox."
-		<p>The proud members of Anime Empire have compiled a list of the entire Code Geass Soundtrack! 
-		Desperate and frustrated looking for lyrics of Code Geass songs all over the web? Your search ends here! 
-		We have the lyrics, names, artists, and downloads available! </p>",
+		<p>The proud members of Anime Empire have compiled a list of the entire Code Geass Soundtrack! Desperate and frustrated looking for lyrics of Code Geass songs all over the web? Your search ends here! If you have any questions or concerns, contact us at our support email in the footer.</p>",
 	'display' => 'Code Geass OSTs',
 	'title' => 'Code Geass OSTs',
 	'link' => $dir.'media/music/ost.php'),
@@ -54,8 +45,7 @@ $section = array(
 		'tags' => 'code geass sound episodes, code geass music',
 		'title' => 'Code Geass Sound Episodes | Code Geass Media'),
 	'leftBox' => "<h2>Sound Episodes</h2><p>This is your one-stop source for 
-		all your <b>Code Geass</b> music related needs. Ever wonder where to find the lyrics of your 
-		favorite song? Then look no further, because we have it all! ",
+		all your <b>Code Geass</b> music related needs. Ever wonder where to find the lyrics of your favorite song? Then look no further, because we have it all! ",
 	'display' => 'Sound Episodes',
 	'title' => 'Code Geass Sound Episodes',
 	'link' => $dir.'media/music/sound.php')
